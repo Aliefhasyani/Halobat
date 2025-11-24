1cb14664d9bcf06f839beab53b0bd31742d55bc1
Yousran <yusranmazidan@gmail.com>
2025-11-24 20:25:14 +0800

    FIX: add BrandController route to API for managing brands

--- Halobat/routes/api.php
+++ Halobat/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ActiveIngredientController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BrandController;
 use App\Http\Controllers\DosageFormController;
 use App\Http\Controllers\DrugController;
 use App\Http\Controllers\ManufacturerController;
@@ -24,4 +25,5 @@ Route::apiResource('users', UserController::class);
 Route::apiResource('manufacturers', ManufacturerController::class);
 Route::apiResource('dosage-forms', DosageFormController::class);
 Route::apiResource('drugs', DrugController::class);
+Route::apiResource('brands', BrandController::class);
 Route::apiResource('active-ingredients', ActiveIngredientController::class);
